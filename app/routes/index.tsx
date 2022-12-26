5167b3a2f38633e620002d9d82f59d52a80d0ee9
7IBO <thibault.fayard.2001@gmail.com>
2022-12-26 15:27:22 +0100

    Remove Prisma dependencies

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,27 +1,11 @@
-import { Project } from "@prisma/client";
 import { json, LoaderFunction } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import Hero from "~/components/Hero";
-import Projects from "~/components/Projects";
-import { createProject, getProjectListItems } from "~/models/project.server";
-import { prisma } from "~/utils/prisma.server";
-
-type LoaderData = {
-  projects: Awaited<ReturnType<typeof getProjectListItems>>;
-};
-
-export const loader: LoaderFunction = async () => {
-  const projects = await getProjectListItems({});
-  return json<LoaderData>({ projects });
-};
 
 const Index = () => {
-  const { projects } = useLoaderData() as LoaderData;
-
   return (
     <>
       <Hero />
-      <Projects projects={projects} />
     </>
   );
 };
